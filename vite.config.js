3f2220ad56d8af73fc0bbadd2ecc38882038044e
Jonathan Langlois <jonathan.langlois@maximuscanada.ca>
2022-03-22 11:52:20 -0700

    npm update and production base path

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,39 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env': process.env
-  },  
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: /^~.+/,
-        replacement: (val) => {
-          return val.replace(/^~/, "");
+export default defineConfig(({mode}) => {
+  const config = {
+    define: {
+      'process.env': process.env
+    },  
+    plugins: [vue()],
+    resolve: {
+      alias: [
+        {
+          find: /^~.+/,
+          replacement: (val) => {
+            return val.replace(/^~/, "");
+          },
         },
-      },
-      {
-        find: '@tailwindConfig',
-        replacement: () => './src/css/tailwind.config.js',
+        {
+          find: '@tailwindConfig',
+          replacement: () => './src/css/tailwind.config.js',
+        }
+      ],
+    },
+    optimizeDeps: {
+      include: [
+        '@tailwindConfig',
+      ]
+    },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
       }
-    ],
-  },
-  optimizeDeps: {
-    include: [
-      '@tailwindConfig',
-    ]
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
     }
   }
+  if (mode === 'production') {
+    config.base = '/vuejs-admin-dashboard-template/'
+  }
+  return config;
 })
